90b027476aeb86adbc5f2bab2279b67f6dbcdf03
Atlai <atlaithewizard@gmail.com>
2026-03-16 12:21:32 +0900

    Create JavaScript file
    Add code to run calculator

---
+++ script.js
@@ -0,0 +1,58 @@
+let currentInput = '';
+let currentOpertation = '';
+let previousInput = '';
+
+function appendNumber(number) {
+  currentInput += number;
+  document.querySelector('.display').textContent = `${previousInput} ${currentOpertation} ${currentInput}`;
+}
+
+function appendOperation(operation) {
+  if (currentInput === '') return;
+  if (previousInput !== '') {
+    calculate();
+  }
+  currentOpertation = operation;
+  previousInput = currentInput;
+  currentInput = '';
+  document.querySelector('.display').textContent = `${previousInput} ${currentOpertation}`;
+}
+
+function calculate() {
+  if (previousInput === '' || currentInput === '') return;
+  let result;
+  let prev = parseFloat(previousInput);
+  let current = parseFloat(currentInput);
+
+  switch (currentOpertation) {
+    case '+':
+      result = prev + current;
+      break;
+    case '-':
+      result = prev - current;
+      break;
+    case '*':
+      result = prev * current;
+      break;
+    case '/':
+      if (current === 0) {
+        alert("That is not currently possible.");
+        return;
+      }
+      result = prev / current;
+      break;
+    default:
+      return;
+  }
+  currentInput = result.toString();
+  currentOpertation = '';
+  previousInput = '';
+  document.querySelector('.display').textContent = currentInput;
+}
+
+function clearDisplay() {
+  currentInput = '';
+  previousInput = '';
+  currentOpertation = '';
+  document.querySelector('.display').textContent = '0';
+}
